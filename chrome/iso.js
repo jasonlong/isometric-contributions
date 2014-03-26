e0bff596f21a4495f1830a862800b5ad8f255ce9
Jason Long <jlong@github.com>
2014-03-26 11:52:37 -0400

    works but w/ timing issues?

--- chrome/iso.js
+++ chrome/iso.js
@@ -1,11 +1,19 @@
+//
+// Inject into GitHub.com
+//
 function icRun() {
-  var target = document.querySelector('.calendar-graph');
+  var target = document.querySelector('.js-calendar-graph');
 
   var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
       if (mutation.type === 'childList') {
-        var days = $('#calendar-graph  rect');
-        d3.selectAll(days).attr("data-contrib-count", function(d, i) { return d[1]; });
+        var maxCount = 0;
+        var days = $('#calendar-graph rect');
+        d3.selectAll(days).attr("data-contrib-count", function(d, i) {
+          if (d[1] > maxCount) maxCount = d[1];
+          return d[1];
+        });
+        target.setAttribute("data-max-contributions", maxCount);
       }
     });
   });
@@ -16,14 +24,21 @@ var script = document.createElement('script');
 script.appendChild(document.createTextNode('('+ icRun +')();'));
 document.documentElement.appendChild(script);
 
+//
+// The actual extension
+//
+
 $(function() {
 
   $('<canvas id="isometric-contributions" width="721" height="500"></canvas>').insertBefore('#contributions-calendar');
 
-  var SIZE      = 10;
-  var GH_OFFSET = 13;
+  var SIZE       = 10;
+  var GH_OFFSET  = 13;
+  var MAX_HEIGHT = 100;
 
   var canvas = document.getElementById('isometric-contributions');
+
+  console.log($('#calendar-graph').length);
 
   // create pixel view container in point
   var point = new obelisk.Point(100, 50);
@@ -36,16 +51,17 @@ $(function() {
   var color3 = new obelisk.CubeColor().getByHorizontalColor(0x44a340);
   var color4 = new obelisk.CubeColor().getByHorizontalColor(0x1e6823);
 
+  var maxContributions = $('.js-calendar-graph').data("max-contributions");
   var contribCount;
 
   $('#calendar-graph g > g').each(function(g) {
     $(this).find('rect').each(function(r) {
-      var r = $(this).get(0);
-      var y = $(this).attr('y') / GH_OFFSET;
-      var style = $(this).attr('style');
-      var contribCount = parseInt($(this).data("contrib-count")) + 3;
+      var r            = $(this).get(0);
+      var y            = $(this).attr('y') / GH_OFFSET;
+      var style        = $(this).attr('style');
+      var contribCount = parseInt($(this).data("contrib-count"));
 
-      var dimension = new obelisk.CubeDimension(SIZE, SIZE, contribCount);
+      var dimension = new obelisk.CubeDimension(SIZE, SIZE, MAX_HEIGHT/maxContributions * contribCount + 3);
 
       if      (style == 'fill: rgb(238, 238, 238);') color = color0;
       else if (style == 'fill: rgb(214, 230, 133);') color = color1;
