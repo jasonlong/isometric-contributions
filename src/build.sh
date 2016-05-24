11d642ca0fe6b6002221200f163c758b5abde541
Jason Long <jlong@github.com>
2016-05-24 13:05:13 -0400

    Add .xpi file (which seems corrupt?)

--- src/build.sh
+++ src/build.sh
@@ -6,4 +6,7 @@ coffee --compile --bare --output firefox/isometric-contributions src/iso.coffee
 coffee --compile --bare --output safari/isometric-contributions.safariextension src/iso.coffee
 
 # Build .xpi for Firefox
-zip -r firefox/isometric-contributions.xpi firefox/isometric-contributions
+cd firefox
+rm isometric-contributions.xpi
+zip -r isometric-contributions.xpi isometric-contributions/
+cd ..
