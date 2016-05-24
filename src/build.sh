5c51f87214aa4c8900a97f45e9a3658f4bbfaeb9
Jason Long <jlong@github.com>
2016-05-24 09:59:45 -0400

    Add FF build command for xpi file

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+# Compile coffeescript
 coffee --compile --bare --output chrome src/iso.coffee
 coffee --compile --bare --output firefox/isometric-contributions src/iso.coffee
 coffee --compile --bare --output safari/isometric-contributions.safariextension src/iso.coffee
+
+# Build .xpi for Firefox
+zip -r firefox/isometric-contributions.xpi firefox/isometric-contributions
