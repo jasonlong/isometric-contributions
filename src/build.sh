8572140385a332d2b7804e35f765e25a2f7b89c6
Jason Long <jlong@github.com>
2016-05-31 08:06:59 -0400

    Fix FF build commands

--- src/build.sh
+++ src/build.sh
@@ -6,7 +6,7 @@ coffee --compile --bare --output firefox/isometric-contributions src/iso.coffee
 coffee --compile --bare --output safari/isometric-contributions.safariextension src/iso.coffee
 
 # Build .xpi for Firefox
-cd firefox
-rm isometric-contributions.xpi
-zip -r isometric-contributions.xpi isometric-contributions/
-cd ..
+cd firefox/isometric-contributions
+rm ../isometric-contributions.xpi
+zip -r ../isometric-contributions.xpi *
+cd ../..
