cdc8785e676a6c3259c5ee67159c20f2cd3f441c
Jason Long <jason@jasonlong.me>
2023-05-06 09:09:43 -0400

    Remove old pretter config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,16 +2,16 @@
  * @type {import('prettier').Options}
  */
 module.exports = {
-  printWidth: 80,
+  printWidth: 120,
   tabWidth: 2,
   useTabs: false,
   semi: false,
-  singleQuote: false,
-  trailingComma: "none",
+  singleQuote: true,
+  trailingComma: 'none',
   bracketSpacing: true,
   bracketSameLine: true,
-  plugins: [require.resolve("@plasmohq/prettier-plugin-sort-imports")],
-  importOrder: ["^@plasmohq/(.*)$", "^~(.*)$", "^[./]"],
+  plugins: [require.resolve('@plasmohq/prettier-plugin-sort-imports')],
+  importOrder: ['^@plasmohq/(.*)$', '^~(.*)$', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true
 }
